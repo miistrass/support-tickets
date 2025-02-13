c6c59b3e2755a58d379cc529a6d02c26439be6f9
Milena Strassburger <milenastrass@gmail.com>
2025-02-13 15:41:56 -0300

    route middleware

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import http from "node:http"
 
 //JS
 import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener(request, response) {
     await jsonHandler(request, response)
+    routeHandler(request, response)
 
 }
 
