ee3e655683b491a789bc94640cc64473032b2835
Milena Strassburger <milenastrass@gmail.com>
2025-02-18 22:21:56 -0300

    getting route parameter

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -8,6 +8,8 @@
  */
 
 export function index({ request, response, database }) {
+    const { status } = request.query
+    console.log(status)
     const tickets = database.select("tickets")
     return response.end(JSON.stringify(tickets))
 }
